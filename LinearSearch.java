a1f93583abc6b5935a3bff426c35ecc0ee77fd15
sachin <sampt1393@gmail.com>
2025-07-29 11:52:31 +0530

    Linearch Search Dynamically

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,27 +1,43 @@
-
-// Linear Search in Java
+import java.util.Scanner;
 
 class LinearSearch {
-  public static int linearSearch(int array[], int x) {
-  int n = array.length;
+    public static int linearSearch(int array[], int x) {
+        int n = array.length;
 
-  // Going through array sequencially
-  for (int i = 0; i < n; i++) {
-    if (array[i] == x)
-    return i;
-  }
-  return -1;
-  }
+        // Going through array sequentially
+        for (int i = 0; i < n; i++) {
+            if (array[i] == x)
+                return i;
+        }
+        return -1;
+    }
 
-  public static void main(String args[]) {
-  int array[] = { 2, 4, 0, 1, 9 };
-  int x = 1;
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
 
-  int result = linearSearch(array, x);
+        // Ask user for number of elements
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
 
-  if (result == -1)
-    System.out.print("Element not found");
-  else
-    System.out.print("Element found at index: " + result);
-  }
+        int array[] = new int[n];
+
+        // Input elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // Ask user for element to search
+        System.out.print("Enter the element to search: ");
+        int x = scanner.nextInt();
+
+        int result = linearSearch(array, x);
+
+        if (result == -1)
+            System.out.print("Element not found");
+        else
+            System.out.print("Element found at index: " + result);
+
+        scanner.close();
+    }
 }
