57785c26631027bd3183832a8a591a87c83755f7
sachin <sampt1393@gmail.com>
2025-07-31 14:30:35 +0530

    Quick Sort Update

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// QuickSort implementation
+class Quicksort1 {
+
+    // method to find the partition position
+    static int partition(int array[], int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                // swap array[i] and array[j]
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        // swap array[i+1] and array[high] (or pivot)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return (i + 1);
+    }
+
+    static void quickSort(int array[], int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+}
+
+// Main class with dynamic input
+public class QuickSort {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Ask for array size
+        System.out.print("Enter the number of elements: ");
+        int size = sc.nextInt();
+
+        int[] data = new int[size];
+
+        // Fill the array with user input
+        System.out.println("Enter " + size + " integer elements:");
+        for (int i = 0; i < size; i++) {
+            data[i] = sc.nextInt();
+        }
+
+        System.out.println("Unsorted Array");
+        System.out.println(Arrays.toString(data));
+
+        // Sort the array
+        Quicksort1.quickSort(data, 0, size - 1);
+
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+
+        sc.close();
+    }
+}
